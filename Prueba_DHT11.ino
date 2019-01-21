272e887ab108ae86ce3aa19c1016fe1fa6600a1a
eSociety97 <46420217+eSociety97@users.noreply.github.com>
2019-01-20 21:29:26 -0600

    Update Prueba_DHT11.ino

--- Prueba_DHT11.ino
+++ Prueba_DHT11.ino
@@ -1,6 +1,6 @@
-/**********************************************Capitulo 12 - Pantalla LCD 2x16*****************************************************
+/**********************************************Capitulo 13 - Sensor DHT11    *****************************************************
 *                                                                                                                                 *
-*     En este doceavo capitulo aprenderemos a utilizar el sensor de DHT11. Este sensor nos permite conocer el valor de la humedad *
+*    En este treceavo capitulo aprenderemos a utilizar el sensor de DHT11. Este sensor nos permite conocer el valor de la humedad *
 *  y la temperatura del ambiente. Este sensor se presume de tener una alta fiabilidad y estabilidad debido a su señal digital     *
 *  Lo podemos encontrar de dos manera con una PCB o solamente el sensor. La diferencia esta en que la PCB nos incluye un led que  *
 *  nos avisa el funcionamiento y una resistencia de 5k en pull-up y otra diferencia son los pines esta cuenta solo con 3 pines y  *
